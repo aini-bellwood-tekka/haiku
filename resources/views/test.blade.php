96a0037c3f40f30d5af9063c05899830f46d9e55
AiNI <aini.aikido@gmail.com>
2018-09-17 18:47:27 +0900

    テスト用更新

---
+++ resources/views/test.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.base')
+@section('title', 'Sample')
+@section('stylesheet')
+    <title>てすと</title>
+    <style>
+    body { color:gray; }
+    h1 { font-size:18pt; font-weight:bold; }
+    th { color:white; background:#999; }
+    td { color:black; background:#eee; padding:5px 10px; }
+    </style>
+@endsection
+@section('content')
+    <h1>はいく</h1>
+    <p>てすと</p>
+@endsection
